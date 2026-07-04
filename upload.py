52e0f1baa463b8185f8b03c2c1f38078ce109b6e
Stoupy51 <stoupy51@gmail.com>
2026-07-05 00:51:22 +0200

    build: 🚀 Upload to GitHub

---
+++ upload.py
@@ -0,0 +1,22 @@
+
+# pyright: reportUnknownVariableType=false
+# Imports
+from beet import ProjectConfig
+from stewbeet import JsonDict
+from stewbeet.continuous_delivery import load_credentials, upload_to_github
+from stewbeet.utils import get_project_config
+
+# Get credentials and try to find the beet configuration
+credentials: dict[str, str] = load_credentials("~/stewbeet/credentials.yml")
+cfg: ProjectConfig = get_project_config()
+
+## Uploads
+# Upload to GitHub
+github_config: JsonDict = {
+    "project_name": "SmithedSummit2026",
+    "version": cfg.version,
+    "build_folder": cfg.output,
+    "endswith": [".zip"]
+}
+changelog: str = upload_to_github(credentials, github_config)
+
