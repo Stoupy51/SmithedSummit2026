5ec5d7bb39b393dbf7be97bb7efc98ef9186ec6d
Stoupy51 <stoupy51@gmail.com>
2026-07-11 13:13:06 +0200

    fix: 🐛 Fixed stickers

--- src/definitions/additions.py
+++ src/definitions/additions.py
@@ -30,6 +30,7 @@ def main():
     }
     Item(id="bg_black_hole", override_model=black_hole_model)
     Item(id="logo")
+    Item(id="logo_sticker")
     Item(id="simplenergy")
     Item(id="switch_minigames")
 
